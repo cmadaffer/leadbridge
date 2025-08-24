b686fb25a1f0feba0cbdb11178af16472cc34ccd
cmadaffer <curtismadaffer@gmail.com>
2025-08-23 21:25:06 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // ensures a Node server build for Render/Vercel
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
